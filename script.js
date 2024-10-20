45dd312253810c9d6027a2e8d08747432b776da4
camila <mariacamilaluango@gmail.com>
2024-10-19 21:30:21 -0500

    desarrollo

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+console.log("area de un triangulo")
+let base= parseInt(prompt("cuanto mide la base?"))
+let alt= parseInt(prompt("cuanto tiene de altura?"))
+let total = (base*alt)/2
 
-
-
+document.write(`el area del triangulo es:  ${total}`)
